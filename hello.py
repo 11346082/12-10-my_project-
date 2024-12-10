6859e3c9ccc02a3b5a71243760e7c1d2d38d95ca
11346082 <11346082@ntub.edu.tw>
2024-12-10 19:36:54 +0800

    修改-程式報告檔案
    新增了一顆愛心

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
 print("Hello yoyoyo")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 定義愛心形狀的參數方程
+t = np.linspace(0, 2 * np.pi, 1000)
+x = 16 * np.sin(t)**3
+y = 13 * np.cos(t) - 5 * np.cos(2*t) - 2 * np.cos(3*t) - np.cos(4*t)
+
+# 繪製愛心
+plt.figure(figsize=(6, 6))
+plt.plot(x, y, color='red')
+plt.title('Heart Shape', fontsize=16)
+plt.axis('off')  # 關閉座標軸
+plt.show()
